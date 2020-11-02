37c4f096eb37bfa96b1ca369632a31ac8d5014a0
shrey <modishrey007@gmail.com>
2020-11-02 23:10:29 +0530

    connected songs and albums to User model

--- fpage/models.py
+++ fpage/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Album(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre =models.CharField(max_length=100)
@@ -12,6 +14,7 @@ class Album(models.Model):
         return self.album_title+'-'+self.artist
 
 class Song(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title= models.CharField(max_length=250)
