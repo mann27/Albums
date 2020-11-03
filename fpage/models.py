2c90bca706e24556aae46bd1127e5154e59ca215
shrey <modishrey007@gmail.com>
2020-11-03 11:42:26 +0530

    fixed related_field issue

--- fpage/models.py
+++ fpage/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Album(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre =models.CharField(max_length=100)
@@ -14,7 +14,7 @@ class Album(models.Model):
         return self.album_title+'-'+self.artist
 
 class Song(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title= models.CharField(max_length=250)
